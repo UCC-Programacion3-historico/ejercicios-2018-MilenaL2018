2d5ea9c0334c3e8e18596aea05cca583c857a2ec
MilenaL2018 <milelucero_09@hotmail.com>
2018-08-08 10:27:33 +0000

    Ejercicio dos y tres listos

--- U00_Repaso/Ej-02/Calculadora.h
+++ U00_Repaso/Ej-02/Calculadora.h
@@ -1,8 +1,29 @@
 #ifndef REPASO_CALCULADORA_H
 #define REPASO_CALCULADORA_H
+#include <iostream>
+using namespace std;
 
-
+template <class T>
 class Calculadora {
+public:
+    T sumar(T a, T b) {
+        return a + b;
+    }
+
+    T restar(T a, T b) {
+        return a - b;
+    }
+
+    T multiplicar(T a, T b) {
+        return a * b;
+    }
+
+    T dividir(T a, T b) {
+      if (b  == 0)
+          throw 404;
+        return a/b;
+    }
+
 
 };
 
--- U00_Repaso/Ej-02/main.cpp
+++ U00_Repaso/Ej-02/main.cpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 int main() {
-    cout << "Ejercicio 00/02\n" << endl;
+    Calculadora <int> micalc;
+    Calculadora <double> micalcDouble;
+
+    cout << micalc.sumar(2,3) << endl;
+    cout << micalc.restar(1,7) << endl;
+    cout << micalc.multiplicar(2,1) << endl;
+
+    try {
+        cout << micalc.dividir(7,0) << endl;
+    } catch (int e) {
+        cout << "Error:" << e << endl;
+    }
+
+    cout << micalcDouble.sumar(0.5,0.07) << endl;
     return 0;
 }
